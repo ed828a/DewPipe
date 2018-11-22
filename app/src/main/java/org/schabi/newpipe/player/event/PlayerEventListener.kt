93ed3ec0115a95aacc0acf1945bbbc6458949252
ed828 <ed828a@gmail.com>
2018-11-22 11:55:04 +1030

    convert PlayerEventListener

---
+++ app/src/main/java/org/schabi/newpipe/player/event/PlayerEventListener.kt
@@ -0,0 +1,13 @@
+package org.schabi.newpipe.player.event
+
+
+import com.google.android.exoplayer2.PlaybackParameters
+
+import org.schabi.newpipe.extractor.stream.StreamInfo
+
+interface PlayerEventListener {
+    fun onPlaybackUpdate(state: Int, repeatMode: Int, shuffled: Boolean, parameters: PlaybackParameters)
+    fun onProgressUpdate(currentProgress: Int, duration: Int, bufferPercent: Int)
+    fun onMetadataUpdate(info: StreamInfo?)
+    fun onServiceStopped()
+}
