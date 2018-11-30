9463b849ef6bc086858d6f0f6ab53d1839b60955
ed828a <ed828a@gmail.com>
2018-11-30 19:40:34 +1030

    convert Constants, FallbackViewHolder

---
+++ app/src/main/java/org/schabi/newpipe/util/FallbackViewHolder.kt
@@ -0,0 +1,6 @@
+package org.schabi.newpipe.util
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class FallbackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
